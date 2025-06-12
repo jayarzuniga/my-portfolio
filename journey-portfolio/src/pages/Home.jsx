30ef154dd4a5f3c019f1dbf613f5f1c87fb841c7
Jay-r <jhayar.zuniga004@gmail.com>
2025-06-12 18:04:18 +0800

    initial template to start my portfolio

---
+++ journey-portfolio/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const Home = () => {
+    return (
+        <Box>
+            <h1>Home</h1>
+        </Box>
+    )
+};
+
+export default Home
